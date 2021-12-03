45b9656fb3583c92f66edd4ca1e1c23223810101
DivBear03 <56279449+DivBear03@users.noreply.github.com>
2021-12-03 14:08:20 -0500

    Update main.cpp

--- 2122_TippingPoint/765A/src/main.cpp
+++ 2122_TippingPoint/765A/src/main.cpp
@@ -284,21 +284,22 @@ void left() {
   setEffectorPositions();
   OdomState goal = drive->getState();
   goal.theta = 90_deg;
-  moveTank(goal, {0, 0, 0}, turnDefault, true);
-  speedMove(500, 0.5);
-  effectors.runOne(GOAL_LIFT, 1);
+  moveTank(goal, {0, 0, 0}, turnDefault, true); // turn left
+  speedMove(500, 0.5);	//backwards
+  effectors.runOne(GOAL_LIFT, 1); //lower goal lift
   pros::delay(2000);
-  speedMove(1300, -0.5);
-	effectors.runOne(GOAL_LIFT, 0);
+  speedMove(1300, -0.5); // move forwards and get goal
+	effectors.runOne(GOAL_LIFT, 0); // raise goal lift
 	pros::delay(500);
-	speedMove(1000, 0.5);
+	speedMove(1000, 0.5); // forwards
 	goal = drive->getState();
   goal.theta = 300_deg;
-	moveTank(goal, {0, 0, 0}, turnDefault, true);
-	fourbarpneum->turnOn();
-   intake->run(true, false, 150);
-  pros::delay(5000);
-  intake->run(true, false, 0);
+	moveTank(goal, {0, 0, 0}, turnDefault, true); // turn towards central mogo
+   intake->run(true, false, 150);  //run intake to deposit rings
+   speedMove(2000, 1);  //move towards
+   fourbarpneum->turnOn(); 
+   speedMove(2000, -1);  //move towards
+	//   intake->run(true, false, 0);  //stop intake
 	
 }
 
