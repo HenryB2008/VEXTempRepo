df0ed187efe4cef2bb8b8a01afca193374d57e1a
sidsrivastava001 <sidsrivastava001@gmail.com>
2022-01-29 13:23:38 -0500

    added third wheel and imu odom

--- 2122_TippingPoint/765A/include/odometry.h
+++ 2122_TippingPoint/765A/include/odometry.h
@@ -1,10 +1,21 @@
 #ifndef _ODOMETRY_
 #define _ODOMETRY_
 
-class Odometry {
-public:
-  Odometry();
+#include "main.h"
+#include "ports.h"
 
-}
+class IMUOdometry {
+public:
+  Odometry(ADIEncoder left, ADIEncoder right, ADIEncoder back, pros::IMU imu, double backdistance, double track);
+  OdomState step();
+private:
+  pros::IMU imu;
+  ADIEncoder left, right, back;
+  int prevleft, prevright, prevback;
+  OdomState prevState;
+
+  
+
+};
 
 #endif
--- 2122_TippingPoint/765A/src/drive.cpp
+++ 2122_TippingPoint/765A/src/drive.cpp
@@ -17,7 +17,12 @@ Drive::Drive() {
               ADIEncoder( // right encoder
                   RIGHT_TRACKING_WHEEL_TOP,
                   RIGHT_TRACKING_WHEEL_BOTTOM
-                  
+
+              ),
+              ADIEncoder( // back  encoder
+                  BACK_TRACKING_WHEEL_TOP,
+                  BACK_TRACKING_WHEEL_BOTTOM
+
               )
           )
           .withOdometry(
