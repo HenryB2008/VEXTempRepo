a751b8f9d6a018b7fdd20eee49514264275c2f49
DivBear03 <56279449+DivBear03@users.noreply.github.com>
2022-01-31 20:51:08 -0500

    path displayer changes

--- 2122_TippingPoint/765A/path_display.py
+++ 2122_TippingPoint/765A/path_display.py
@@ -1,8 +1,8 @@
 import PIL 
 from PIL import Image, ImageDraw
 import matplotlib
-# import re 
-
+import re 
+import math
 # function = input("Enter function name: ")
 
 functiontext = ""
@@ -23,10 +23,10 @@ with open("src/main.cpp", "r+") as texthandle:
     movements = []
     for command in commands:
         command = command.strip()
-        if command.startswith("distanceMove") or command.startswith("dragTurn") or command.startswith("moveTank"):
+        if command.startswith("distanceMove") or command.startswith("dragTurn") or command.startswith("goal.theta"):
             movements.append(command)
-    for movement in movements:
-        print(movement)
+    # for movement in movements:
+        # print(movement)
     
     #635 width for 12 ft
     #52.9 px / ft and 4.4 px / inch
@@ -34,8 +34,36 @@ with open("src/main.cpp", "r+") as texthandle:
     base_image = Image.open("skills_field.PNG")
     drawing = ImageDraw.Draw(base_image)
     drawing.ellipse([(70,70),(100,100)], "black", "black")
-    currentheading = 
+    currentheading = 0
+    currentmovement = 1
+    oldx = 85
+    oldy = 85
+    n = 1
     for movement in movements:
         if movement.startswith("distanceMove"):
+            arguments = movement[movement.index("(")+1:movement.index(")")].replace(" ", "").split(",")
+            # arguments = arguments.replace(" ", "")
+            # arguments = arguments.split(",")
+            # print(arguments)
+            distance = int(arguments[0])
+            direction = float(arguments[1])
+            if direction < 0:
+                currentmovement = -1
+                newx = oldx + (distance * 4.4 * math.cos(math.pi / 2 + ((90-currentheading) * math.pi / 180)))
+                newy = oldy + (distance * 4.4 * math.sin(math.pi / 2 + ((90-currentheading) * math.pi / 180)))  
+            else:
+                currentmovement = 1
+                newx = oldx + (distance * 4.4 * math.cos((90-currentheading) * math.pi / 180))
+                newy = oldy + (distance * 4.4 * math.sin((90-currentheading) * math.pi / 180))
+            
+            drawing.line([(oldx, oldy),(newx,newy)], "black", 2)
+            oldx = newx
+            oldy = newy
+            # direction = int(arguments[1])
 
+        if movement.startswith("goal.theta"):
+            currentheading = int(movement[movement.index("=")+2:movement.index("_")].replace(" ", ""))
+        n += 1
+        if n > 2:
+            break
     base_image.show()
