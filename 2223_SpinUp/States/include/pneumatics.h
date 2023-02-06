a956ead3e933ff19af995712e5e1ff0a756643e9
therealdeal201 <67665796+therealdeal201@users.noreply.github.com>
2023-02-05 22:58:28 -0500

    started new project

---
+++ 2223_SpinUp/States/include/pneumatics.h
@@ -0,0 +1,40 @@
+#ifndef PNEUMATICS_H
+#define PNEUMATICS_H
+
+#include "main.h"
+
+// Enum representeing if a pneumatic is turned on or not
+enum PneumaticState {
+
+    DEACTIVATED,
+    ACTIVATED
+
+};
+
+// Class for pneumatic actuation
+class Pneumatics {
+
+    private:
+
+        // ADiDigitalOut has 2 modes - on or off
+        pros::ADIDigitalOut piston;
+
+        // Stores the current state of the piston, with false being off
+        bool state = false;
+
+    public:
+
+        // Constructor takes in an unsigned int
+        Pneumatics(uint8_t port);
+
+        // Activates pneumatic
+        void on();
+
+        // Deactivates pneumatoc
+        void off();
+
+        bool getPreviousState
+
+};
+
+#endif
