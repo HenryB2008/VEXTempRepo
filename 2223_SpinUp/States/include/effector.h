20b5e96ae90d97a4795e80ed12da9ee0ebaab6e7
therealdeal201 <67665796+therealdeal201@users.noreply.github.com>
2023-02-18 18:40:15 -0500

    states changes

---
+++ 2223_SpinUp/States/include/effector.h
@@ -0,0 +1,32 @@
+#ifndef EFFECTOR_H
+#define EFFECTOR_H
+
+#include "main.h"
+#include "okapi/api/device/motor/abstractMotor.hpp"
+#include "okapi/impl/device/motor/motorGroup.hpp"
+#include "ports.h"
+
+using okapi::MotorGroup;
+
+class Effector {
+    private:
+        MotorGroup motors;
+
+        const std::vector<double> powers;
+
+        int currPowerIndex = -1;
+        Direction prevDir;
+
+    public:
+        Effector(const MotorGroup& motors, const std::vector<double>& powers);
+        void runVoltage(double power);
+        void runRPM(double rpm);
+        void runTimed(double power, double time);
+        void toggle(const Direction& dir = FORWARD);
+        void runWhenPressed(const okapi::ControllerDigital& b, double power);
+};
+
+extern Effector intake;
+extern Effector flywheel;
+
+#endif
---
+++ 2223_SpinUp/States/src/controller.cpp
@@ -0,0 +1,65 @@
+#include "controller.h"
+
+namespace Controller {
+
+    // initialize the controller and deadzone
+    okapi::Controller masterController(okapi::ControllerId::master);
+    constexpr double deadzone = 0.1;
+
+    // initialize the map of buttons
+    std::unordered_map<okapi::ControllerDigital, ToggleData> toggleMap = {
+        { INTAKE_IN, { false, [](){ intake.toggle(FORWARD); } } },
+        { INTAKE_OUT, { false, [](){ intake.toggle(REVERSE); } } }
+    };
+
+    std::unordered_map<okapi::ControllerDigital, HoldData> holdMap = {
+        // { RUN_INDEXER, { [](){ indexer.runWhenPressed(RUN_INDEXER, 12000); }, [](){ indexer.runWhenPressed(RUN_INDEXER, 0); } } }
+    };
+
+    void addToggleControl(okapi::ControllerDigital btn, std::function<void()> func) {
+        toggleMap.insert({ btn, { false, func }});
+    }
+
+    bool toggleContains(okapi::ControllerDigital btn) {
+        return toggleMap.count(btn) > 0;
+    }
+
+    void step() { 
+        // goes through each button and iterates the count if the button is currently pressed (one time)
+        //std::cout << "Starting controller step\n";
+        for (auto& [ key, value ] : toggleMap) {
+            bool currentState = masterController.getDigital(key); // current state of the button
+
+            if (currentState && !value.previousState) // debouncing
+                value.callback(); // runs the callback function
+
+            value.previousState = currentState;
+        }
+
+        // handle the hold map
+        for (auto& [ key, value ] : holdMap) {
+            if (masterController.getDigital(key))
+                value.on();
+            else
+                value.off();
+        }
+    }
+
+    double getForward() {
+        double forward = masterController.getAnalog(FORWARD_CONTROL);
+
+        // utilizing deadzone
+        return fabs(forward) > deadzone ? forward : 0;
+    }
+
+    double getYaw() {
+        double yaw = masterController.getAnalog(YAW_CONTROL);
+
+        // utilizing deadzone
+        return fabs(yaw) > deadzone ? yaw : 0;
+    }
+
+    bool getDigital(const okapi::ControllerDigital button) {
+        return masterController.getDigital(button);
+    }
+};
