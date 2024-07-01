30e7935b0751e809a1fbd5fbcfb27a562a91fc8d
HenryB2008 <henrybagdas@gmail.com>
2024-07-01 11:29:35 -0400

    minor prog change

--- 2425_HighStakes/include/controller.h
+++ 2425_HighStakes/include/controller.h
@@ -8,4 +8,3 @@
 extern pros::Controller controller;
 
 
-void opcontrol();
--- 2425_HighStakes/src/controller.cpp
+++ 2425_HighStakes/src/controller.cpp
@@ -5,37 +5,3 @@ using namespace pros;
 pros::Controller controller(pros::E_CONTROLLER_MASTER);
 
 
-void opcontrol() {
-    // loop forever
-    while (true) {
-        // get left y and right y positions
-        int leftY = controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
-        int rightY = controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y);
-        int R1Button = controller.get_digital(E_CONTROLLER_DIGITAL_R1); 
-        int R2Button = controller.get_digital(E_CONTROLLER_DIGITAL_R2); 
-        
-        
-
-        // move the robot
-        chassis.tank(leftY, rightY);
-
-
-        //intake code
-        if(R2Button)
-        {
-            IntakeMotor1.move(128);
-            IntakeMotor2.move(-128);
-        } else if (R1Button)
-        {
-            IntakeMotor1.move(-128);
-            IntakeMotor2.move(128);
-        } else
-        {
-            IntakeMotor1.brake();
-            IntakeMotor2.brake(); 
-        }
-        
-        // delay to save resources
-        pros::delay(25);
-    }
-}
