74c90859e7846908b67ca82df96ee96e5c994275
HenryB2008 <henrybagdas@gmail.com>
2024-12-22 19:09:51 -0500

    stuff for raney

--- 2425_HighStakes/src/main.cpp
+++ 2425_HighStakes/src/main.cpp
@@ -3,8 +3,6 @@
 #include "pros/misc.h"
 using namespace pros; 
 
-ASSET(firstRing_txt);
-ASSET(followRoute_txt);
 
 
 
@@ -28,9 +26,9 @@ lemlib::OdomSensors sensors(&vertical_tracking_wheel, // Vertical tracking wheel
 
 //PID-
 // latetral PID controller
-lemlib::ControllerSettings lateral_controller(5.9, // proportional gain (kP)
+lemlib::ControllerSettings lateral_controller(6.2, // proportional gain (kP)
                                               0, // integral gain (kI)
-                                              2.2, // derivative gain (kD)
+                                              1.9, // derivative gain (kD)
                                               0, // anti windup
                                               0, // small error range, in inches
                                               0, // small error range timeout, in milliseconds
@@ -102,7 +100,7 @@ int liftPrevError = 0;
 int liftDerivative; 
 int liftTotalError = 0; 
 
-bool enableLiftPID = true; 
+
 
 
 
@@ -134,29 +132,25 @@ void liftPIDTask(void* param) {
     }
 }
 
+bool intakeRunning = false; 
+bool reverse = false; 
 
-int distance; 
-bool monitorDistanceRunning = false; 
-void monitorDistance(void* param) {
-    
-  while (true) {
-    
-    if(monitorDistanceRunning) {
-        distance = intakeDistance.get_proximity(); 
-        
-        if(distance == 255) {
-            monitorDistanceRunning = false;
-            pros::delay(625);
-            IntakeMotor1.brake(); 
-            
+void intakeTask(void* param) {
+    while (true) {
+        if(intakeRunning && reverse) {
+            IntakeMotor1.move(127);
+        } else if (intakeRunning && reverse==false) {
+            IntakeMotor1.move(-127); 
         } else {
-            IntakeMotor1.move(-127);    
+            IntakeMotor1.brake(); 
         }
-        
+        pros::delay(20);
     }
-    pros::delay(5);
-  }
 }
+
+
+ 
+
 
 
 
@@ -220,6 +214,9 @@ void opcontrol() {
         } else if (controller.get_digital_new_press(E_CONTROLLER_DIGITAL_L1)) {
             desiredLiftValue = 32000;
             liftPIDRunning = true;
+            IntakeMotor1.move(-127);
+            pros::delay(1000);
+            IntakeMotor1.brake();  
         }
         
     
@@ -243,139 +240,67 @@ void opcontrol() {
 
 
 
-void test() {
+void red(){
     
-    chassis.setPose(-11, 50, 235);
-    pros::delay(10); 
-    chassis.follow(followRoute_txt, 8, 2500);
-    
-    
-    
-}
-
-
-void blueRight() {
-    chassis.setPose(0,0,0); //set initial position
-    liftPIDRunning = true; 
-    desiredLiftValue = 22800; 
-    doinker.extend();       
-    chassis.moveToPoint(0,39.5,  2000);   //move up to knock the pair of blue rings off
-    chassis.waitUntil(38.7);            
-    chassis.cancelMotion();
-    pros::delay(175);
-    chassis.moveToPoint(0,29,800, {.forwards = false});
-    chassis.waitUntil(10.3); 
-    chassis.cancelMotion(); 
-    doinker.retract();
-    pros::delay(300);
-    chassis.turnToHeading(62.5, 700); 
-    chassis.waitUntil(61.5);
-    chassis.cancelMotion();
-    pros::delay(20);    
-    chassis.setPose(24, 30, 0);
-    pros::delay(250);
-    chassis.moveToPoint(24, 19, 800, {.forwards = false, .minSpeed = 10, .earlyExitRange = 0.8, }, false);
-    mogo.toggle();  
-    pros::delay(20);
-    IntakeMotor1.move(-127);
+    chassis.setPose(0,0,0);     //calibrate position  
+    liftPIDRunning = true;  
+    desiredLiftValue = 22800;       //lift arm for intakex   
+    pros::delay(10);             
+    chassis.moveToPoint(0,-8, 1200, {.forwards = false, .minSpeed = 72}, false); //move up to edge the other bot
+    pros::delay(100);
+    chassis.moveToPoint(0,4,1200, {}, false);           //move back to the original position
     pros::delay(200);
-    chassis.turnToHeading(-55, 800, {.minSpeed=15, .earlyExitRange = 0.9}, false);
-    chassis.setPose(0,0,0);
-    pros::delay(100);
-    chassis.moveToPoint(0, 14   , 1500, {.minSpeed = 10, .earlyExitRange = 0.8}, false);
-    pros::delay(100);
-    chassis.turnToHeading(52.5, 1000, {.minSpeed=15, .earlyExitRange = 0.9}, false);
-    pros::delay(100);
-    chassis.setPose(0,0,0);
-    pros::delay(10);
-    chassis.moveToPoint(0,8,1000, {.maxSpeed = 40, .minSpeed = 10, .earlyExitRange = 0.8}, false);
-    pros::delay(500);
-    chassis.turnToHeading(-165, 800, {}, false); 
-    chassis.waitUntilDone();
-    chassis.setPose(0,0,0);
-    pros::delay(50);
-    chassis.moveToPoint(0,32, 1500, {.maxSpeed = 80}, false);
-
-}
-
-
-void redLeft()
-{   
-    chassis.setPose(0,0,0); //set initial position
-    liftPIDRunning = true; 
-    desiredLiftValue = 22800; 
-    doinker.extend();       
-    chassis.moveToPoint(0,39.5,  2000);   //move up to knock the pair of blue rings off
-    chassis.waitUntil(38.7);            
-    chassis.cancelMotion();
-    pros::delay(175);
-    chassis.moveToPoint(0,29,800, {.forwards = false});
-    chassis.waitUntil(10.3); 
-    chassis.cancelMotion(); 
-    doinker.retract();
-    pros::delay(300);
-    chassis.turnToHeading(-62.5, 700); 
-    chassis.waitUntil(61.5);
-    chassis.cancelMotion();
-    pros::delay(20);    
-    chassis.setPose(-24, 30, 0);
-    pros::delay(250);
-    chassis.moveToPoint(-24, 19, 800, {.forwards = false, .minSpeed = 10, .earlyExitRange = 0.8, }, false);
-    mogo.toggle();  
-    pros::delay(20);
-    IntakeMotor1.move(-127);
+    chassis.turnToHeading(304, 1200, {}, false);         //turn to face the mogo
     pros::delay(200);
-    chassis.turnToHeading(55, 800, {.minSpeed=15, .earlyExitRange = 0.9}, false);
     chassis.setPose(0,0,0);
-    pros::delay(100);
-    chassis.moveToPoint(0, 14   , 1500, {.minSpeed = 10, .earlyExitRange = 0.8}, false);
-    pros::delay(100);
-    chassis.turnToHeading(-52.5, 1000, {.minSpeed=15, .earlyExitRange = 0.9}, false);
-    pros::delay(100);
-    chassis.setPose(0,0,0);
-    pros::delay(10);
-    chassis.moveToPoint(0,8,1000, {.maxSpeed = 40, .minSpeed = 10, .earlyExitRange = 0.8}, false);
-    pros::delay(500);
-    chassis.turnToHeading(-165, 800, {}, false); 
-    chassis.waitUntilDone();
-    chassis.setPose(0,0,0);
-    pros::delay(50);
-    chassis.moveToPoint(0,32, 1500, {.maxSpeed = 80}, false);
-    
-    /*
-    chassis.turnToHeading(-115, 800, {.minSpeed=15, .earlyExitRange = 0.9}, false);
     pros::delay(20);
-    liftPIDRunning = true; 
-    desiredLiftValue = 21200; 
-    pros::delay(250);
-    chassis.setPose(0,0,0);
+    chassis.moveToPoint(0, -30, 1400, {.forwards = false, .maxSpeed=90, .minSpeed = 1,  .earlyExitRange = 1});          
+    chassis.waitUntil(16);
+    intakeRunning = true; 
+    chassis.waitUntilDone(); 
+    pros::delay(50);
+    mogo.toggle(); 
+    pros::delay(300); 
+    chassis.turnToHeading(243, 1200, {}, false);         //-28.365, -20.951, 180
+    pros::delay(10);            
+    chassis.setPose(0,0,0); 
+    pros::delay(150);
+    chassis.moveToPoint(0, 26, 900, {}, false);     //y: -44.951 
+    pros::delay(1000);
+    chassis.turnToHeading(159, 800, {}, false);
     pros::delay(10);
-    chassis.moveToPoint(0, 20, 1500, {.minSpeed=72, .earlyExitRange = 1}, false);
-    mogo.toggle();
-    monitorDistanceRunning = true; 
-    chassis.moveToPoint(0, 57, 1500, {.minSpeed=10, .earlyExitRange = 1});
-    if(!chassis.isInMotion())
-    {
-        pros::delay(200);   
-    }
-    */
+    chassis.setPose(0,0,0);  //theta 337 
+    mogo.toggle(); 
+    pros::delay(500);
+    reverse = true; 
+    chassis.moveToPoint(0, 55, 1200, {.maxSpeed = 110}, false);    //-38.274, 0.434, 329 
+    pros::delay(10);
+    chassis.setPose(0,0,0);
+    pros::delay(300);
+    chassis.turnToHeading(257, 800, {}, false);
+    pros::delay(10);
+    chassis.setPose(0,0,0);     
+    pros::delay(200);
+    chassis.moveToPoint(0,-16, 800, {.forwards = false}, false);     //gets the second mogo 
+    mogo.toggle(); 
+    pros::delay(250);
+    chassis.turnToHeading(125, 800, {}, false); 
+    chassis.setPose(0,0,0);
+    pros::delay(300); 
+    reverse = false; 
+    chassis.moveToPoint(0, 28, 900);
 
 
 
-    //chassis.turnToHeading(28, 800, {.minSpeed=15, .earlyExitRange = 0.9}, false);
-    
-    
-
-
-
-    
+    pros::delay(2000);
     
 
 }
+    
+    
 
 void autonomous() {
-    redLeft(); 
-
+    red(); 
 }
 
 
@@ -388,16 +313,14 @@ void initialize() {
     pros::lcd::initialize(); // initialize brain screen
     chassis.setBrakeMode(pros::E_MOTOR_BRAKE_COAST);    
     chassis.calibrate(); // calibrate sensors
-    rotation_sensor.set_data_rate(5);
     LiftMotor.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
     
 
-
-
-   
     pros::Task liftPIDController(liftPIDTask, nullptr, "Lift PID Task");
-    pros::Task distanceTask(monitorDistance, nullptr, "Intake Distance Task"); 
-    
+    pros::Task intakeStuff(intakeTask, nullptr, "intake Task");
+
+
+
 
     //pros::Task toggleDoinker(toggleDoinkerTask, nullptr, "Toggle Doinker");
 
@@ -415,7 +338,7 @@ void initialize() {
 
         }
     });    
-    autonomous(); 
+    opcontrol(); 
 }
 
 
